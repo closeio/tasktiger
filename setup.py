f5f159e464efc766cbfc5a6bfb1d4ac9b40b6c9f
Thomas Steinacher <tom@eggdrop.ch>
2015-08-15 00:35:21 -0700

    setup.py + move files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='tasktiger',
+    version='0.1',
+    url='http://github.com/closeio/tasktiger',
+    license='MIT',
+    description='Python task queue',
+    #test_suite='tests',
+    #tests_require=['redis'],
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+)
