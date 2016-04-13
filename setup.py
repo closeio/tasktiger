a7e4366207f9bdecd627bd491160625deba49ebd
Thomas Steinacher <tom@eggdrop.ch>
2016-04-13 13:23:28 +0200

    Add additional Python classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ],
     packages=[
         'tasktiger',
