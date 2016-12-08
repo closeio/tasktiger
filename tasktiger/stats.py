5124b8e04edca0c4ad86f85cdea568d8b8968617
Thomas Steinacher <tom@eggdrop.ch>
2016-12-07 23:17:38 -0800

    Fix race condition in stats: _task_start_time may be set to None in report_task_end() when report_task_start() and compute_stats() run at the same time

--- tasktiger/stats.py
+++ tasktiger/stats.py
@@ -12,27 +12,38 @@ class StatsThread(threading.Thread):
         self._time_busy = 0
         self._task_start_time = None
 
+        # Lock that protects stats computations from interleaving. For example,
+        # we don't want report_task_start() to run at the same time as
+        # compute_stats(), as it might result in an inconsistent state.
+        self._computation_lock = threading.Lock()
+
     def report_task_start(self):
-        self._task_start_time = time.time()
-        self._task_running = True
+        now = time.time()
+        with self._computation_lock:
+            self._task_start_time = now
+            self._task_running = True
 
     def report_task_end(self):
         now = time.time()
-        self._time_busy += now - self._task_start_time
-        self._task_running = False
-        self._task_start_time = None
+        with self._computation_lock:
+            self._time_busy += now - self._task_start_time
+            self._task_running = False
+            self._task_start_time = None
 
     def compute_stats(self):
         now = time.time()
-        time_total = now - self._time_start
-        time_busy = self._time_busy
-        self._time_start = now
-        self._time_busy = 0
-        if self._task_running:
-            time_busy += now - self._task_start_time
-            self._task_start_time = now
-        else:
-            self._task_start_time = None
+
+        with self._computation_lock:
+            time_total = now - self._time_start
+            time_busy = self._time_busy
+            self._time_start = now
+            self._time_busy = 0
+            if self._task_running:
+                time_busy += now - self._task_start_time
+                self._task_start_time = now
+            else:
+                self._task_start_time = None
+
         if time_total:
             utilization = 100. / time_total * time_busy
             with g_fork_lock:
