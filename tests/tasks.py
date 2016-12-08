3c92261b375d77657bd9b93afb1b4556618ca56c
Thomas Steinacher <tom@eggdrop.ch>
2016-12-07 16:39:13 -0800

    Ensure that a task object that disappears while the task is processing is handled properly. This could happen when a worker processes a task, then hangs for a long time, causing another worker to pick up and finish the task. Then, when the original worker resumes, the task object will be gone. Make sure we log a "not found" error and move on.

--- tests/tasks.py
+++ tests/tasks.py
@@ -101,5 +101,5 @@ def verify_current_tasks(tasks):
         conn.rpush('task_ids', *[t.id for t in tasks])
 
 @tiger.task()
-def sleep_task():
-    time.sleep(10)
+def sleep_task(delay=10):
+    time.sleep(delay)
