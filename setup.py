3ad51270299123a336116ab238ef2391a903dbb3
Thomas Steinacher <tom@eggdrop.ch>
2016-12-08 16:04:25 -0800

    v0.8.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tasktiger',
-    version='0.8.2',
+    version='0.8.3',
     url='http://github.com/closeio/tasktiger',
     license='MIT',
     description='Python task queue',
