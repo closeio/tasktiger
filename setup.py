60362fd610258fca185fd9c8b234f2367cf50065
Thomas Steinacher <tom@eggdrop.ch>
2017-05-24 13:47:13 -0700

    v0.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require = install_requires + [
 
 setup(
     name='tasktiger',
-    version='0.8.5',
+    version='0.9',
     url='http://github.com/closeio/tasktiger',
     license='MIT',
     description='Python task queue',
