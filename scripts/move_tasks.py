4fdb9ab458d0d4d0c8cca30e85c8c3230474bcd3
Joe Kemp <joe@close.io>
2018-10-16 09:57:46 -0400

    Replace dump/restore commands

--- scripts/move_tasks.py
+++ scripts/move_tasks.py
@@ -11,39 +11,38 @@ PREFIX = 't'
 
 def move_tasks(source, dest, state, queue, apply):
     """Move tasks in a queue."""
-    key = '{}:{}:{}'.format(PREFIX, state, queue)
-    tasks = source.zrange(key, 0, -1)
-    dump = source.dump(key)
+    queue_key = '{}:{}:{}'.format(PREFIX, state, queue)
+    tasks = source.zrange(queue_key, 0, -1, withscores=True)
     if apply:
-        print '  Moving tasks'
-        dest.restore(key, 0, dump)
+        print '  Moving tasks {}'.format(queue_key)
 
     for task in tasks:
-        key = '{}:task:{}'.format(PREFIX, task)
-        dump = source.dump(key)
+        key = '{}:task:{}'.format(PREFIX, task[0])
+        task_data = source.get(key)
         key_executions = '{}:executions'.format(key)
-        dump_executions = source.dump(key_executions)
+        executions_data = source.lrange(key_executions, 0, -1)
         print '  task', queue, task
         if apply:
             print '    Moving'
-            dest.restore(key, 0, dump)
-        if dump_executions is not None and apply:
-            print '    Moving executions'
-            dest.restore(key_executions, 0, dump_executions)
+            dest.set(key, task_data)
+            dest.zadd(queue_key, task[0], task[1])
+        if executions_data is not None and apply:
+            print '    Moving executions {}'.format(key_executions)
+            for execution in executions_data:
+                dest.rpush(key_executions, execution)
 
 
 def move_queues_in_state(source, dest, state, apply):
     """Move queues in a specific state."""
     print 'queues', state
     key = '{}:{}'.format(PREFIX, state)
-    dump = source.dump(key)
-    if dump is not None and apply:
-        print 'Moving queue'
-        dest.restore(key, 0, dump)
 
     queues = source.smembers(key)
     for queue in queues:
         move_tasks(source, dest, state, queue, apply)
+        if apply:
+            print 'Moving queue {} {}'.format(key, queue)
+            dest.sadd(key, queue)
 
 
 @click.command()
@@ -55,7 +54,7 @@ def run(apply=False):
     move_queues_in_state(source, dest, 'queued', apply)
     move_queues_in_state(source, dest, 'active', apply)
     move_queues_in_state(source, dest, 'scheduled', apply)
-    # move_queues_in_state(source, dest, 'error', apply)
+    move_queues_in_state(source, dest, 'error', apply)
 
 
 if __name__ == '__main__':
