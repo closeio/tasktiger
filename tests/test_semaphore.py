8bf94f57cb866679c1bce3fd15eec24dbd9ef548
Joe Kemp <joe@close.io>
2018-11-15 14:03:13 -0500

    Cleanup

--- tests/test_semaphore.py
+++ tests/test_semaphore.py
@@ -1,3 +1,4 @@
+"""Test Redis Semaphore lock."""
 import datetime
 
 from freezefrog import FreezeTime
@@ -7,21 +8,27 @@ from .utils import get_tiger
 
 
 class TestSemaphore:
+    """Test Redis Semaphores."""
+
     def setup_method(self, method):
+        """Test setup."""
+
         self.tiger = get_tiger()
         self.conn = self.tiger.connection
         self.conn.flushdb()
 
     def teardown_method(self, method):
+        """Test teardown."""
+
         self.conn.flushdb()
 
-    def test_semaphore(self):
+    def test_simple_semaphore(self):
+        """Test semaphore."""
+
         semaphore1 = Semaphore(self.conn, 'test_key',
-                                    'id_1', max=1,
-                                    timeout=10)
+                               'id_1', max=1, timeout=10)
         semaphore2 = Semaphore(self.conn, 'test_key',
-                               'id_2', max=1,
-                               timeout=10)
+                               'id_2', max=1, timeout=10)
 
         # Get lock and then release
         with FreezeTime(datetime.datetime(2014, 1, 1)):
@@ -30,13 +37,13 @@ class TestSemaphore:
         assert locks == 1
         semaphore1.release()
 
-        # Get lock
+        # Get a new lock after releasing old
         with FreezeTime(datetime.datetime(2014, 1, 1)):
             acquired, locks = semaphore2.acquire()
         assert acquired
         assert locks == 1
 
-        # Fail getting lock
+        # Fail getting second lock while still inside time out period
         with FreezeTime(datetime.datetime(2014, 1, 1, 0, 0, 9)):
             acquired, locks = semaphore1.acquire()
         assert not acquired
@@ -48,7 +55,7 @@ class TestSemaphore:
         assert acquired
         assert locks == 1
 
-    def test_semaphores_multiple(self):
+    def test_multiple_locks(self):
         semaphore1 = Semaphore(self.conn, 'test_key',
                                'id_1', max=2,
                                timeout=10)
@@ -59,16 +66,17 @@ class TestSemaphore:
                                'id_3', max=2,
                                timeout=10)
 
+        # First two locks should be acquired
         with FreezeTime(datetime.datetime(2014, 1, 1)):
             acquired, locks = semaphore1.acquire()
         assert acquired
         assert locks == 1
-
         with FreezeTime(datetime.datetime(2014, 1, 1, 0, 0, 4)):
             acquired, locks = semaphore2.acquire()
         assert acquired
         assert locks == 2
 
+        # Third lock should fail
         with FreezeTime(datetime.datetime(2014, 1, 1, 0, 0, 6)):
             acquired, locks = semaphore3.acquire()
         assert not acquired
@@ -76,6 +84,7 @@ class TestSemaphore:
 
         semaphore2.release()
 
+        # Releasing one of the existing locks should let a new lock succeed
         with FreezeTime(datetime.datetime(2014, 1, 1, 0, 0, 9)):
             acquired, locks = semaphore3.acquire()
         assert acquired
