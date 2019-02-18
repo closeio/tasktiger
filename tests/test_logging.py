bb3ca52921b3258546e7d818768af4b93a9c13b0
Joe Kemp <joe@six7.io>
2019-02-18 15:05:00 -0500

    Fix logging import and add test (#128)

---
+++ tests/test_logging.py
@@ -0,0 +1,58 @@
+import structlog
+
+from .test_base import BaseTestCase
+
+from tasktiger import Worker
+from tasktiger.logging import tasktiger_processor
+
+from .utils import get_tiger
+
+tiger = get_tiger()
+
+logger = structlog.getLogger("tasktiger")
+
+
+def logging_task():
+    log = logger.info("simple task")
+    # Confirm tasktiger_processor injected task id
+    assert log[1]["task_id"] == tiger.current_task.id
+
+
+class TestLogging(BaseTestCase):
+    """Test logging."""
+
+    def test_structlog_processor(self):
+        try:
+            # Use ReturnLogger for testing
+            structlog.configure(
+                processors=[tasktiger_processor],
+                context_class=dict,
+                logger_factory=structlog.ReturnLoggerFactory(),
+                wrapper_class=structlog.stdlib.BoundLogger,
+                cache_logger_on_first_use=True,
+            )
+
+            # Run a simple task. Logging output is verified in
+            # the task.
+            self.tiger.delay(logging_task)
+            queues = self._ensure_queues(queued={"default": 1})
+            task = queues["queued"]["default"][0]
+            assert task["func"] == "tests.test_logging:logging_task"
+            Worker(self.tiger).run(once=True)
+            self._ensure_queues(queued={"default": 0})
+            assert not self.conn.exists("t:task:%s" % task["id"])
+        finally:
+            structlog.configure(
+                processors=[
+                    structlog.stdlib.add_log_level,
+                    structlog.stdlib.filter_by_level,
+                    structlog.processors.TimeStamper(fmt="iso", utc=True),
+                    structlog.processors.StackInfoRenderer(),
+                    structlog.processors.format_exc_info,
+                    structlog.processors.JSONRenderer(),
+                ],
+                context_class=dict,
+                logger_factory=structlog.ReturnLoggerFactory(),
+                wrapper_class=structlog.stdlib.BoundLogger,
+                cache_logger_on_first_use=True,
+            )
