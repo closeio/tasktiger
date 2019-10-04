2685c897f71463aeb91fe74b89a91e0eaa7100d7
Alec Rosenbaum <alec.rosenbaum@close.io>
2019-10-04 10:11:20 -0400

    pull redis host from environment, use requirements file

--- tests/config.py
+++ tests/config.py
@@ -1,8 +1,10 @@
+import os
+
 # How much to delay scheduled tasks for testing purposes.
 DELAY = 0.2
 
 # Redis database number which will be wiped and used for the tests
-TEST_DB = 7
+TEST_DB = int(os.environ.get('REDIS_DB', 7))
 
 # Redis hostname
-REDIS_HOST = 'redis'
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
