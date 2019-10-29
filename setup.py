5caa4974fac5deddca29747fa05aabeee834d749
Joe Kemp <joe@close.io>
2019-10-29 15:25:45 -0400

    v0.10.1 (#153)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = install_requires + ['freezefrog', 'pytest', 'psutil']
 
 setup(
     name='tasktiger',
-    version='0.9.6',
+    version='0.10.1',
     url='http://github.com/closeio/tasktiger',
     license='MIT',
     description='Python task queue',
