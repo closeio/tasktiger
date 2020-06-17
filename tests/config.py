9d42fe71e290874ec7afec4c40a77ba96c4bed8a
Thomas Steinacher <tom@eggdrop.ch>
2020-06-17 11:06:46 -0400

    Fix tests timings (#164)
    * Ąssumes processes take longer to start on  macOS Catalina
    
        See also https://news.ycombinator.com/item?id=23273247
    
        I confirmed using the commands from https://news.ycombinator.com/item?id=23281106 that it is indeed being held up by Catalina's security.
    
        This allows for enough time to run tests on macOS Catalina on a 2020 MacBook Pro with 2.3 GHz Quad-Core i7.
    
        Since many tests rely on the DELAY, and occasionally 0.2s caused issues on Circle as well, it didn't make sense to introduce a setting just for spawning the process, so I decided to increase the wait time for all tests. This doesn't seem to have a significant impact on the overall test execution time in Circle.
    
    * Fixes timing issue in TestPeriodicTasks.test_periodic_execution. This issue happens specifically when at test start time less than 0.2 seconds are left until the next second starts, and the worker doesn't have enough time to queue periodic tasks.

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,10 @@
 import os
 
 # How much to delay scheduled tasks for testing purposes.
-DELAY = 0.2
+# Note that on macOS Catalina, when using an unsigned Python version, taskgated
+# (com.apple.securityd) needs to approve launching the process. We therefore
+# need ample time here (> 0.3s) in order to prevent test failures.
+DELAY = 0.4
 
 # Redis database number which will be wiped and used for the tests
 TEST_DB = int(os.environ.get('REDIS_DB', 7))
