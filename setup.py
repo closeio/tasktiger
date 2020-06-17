2cc33a5a0c59141ca96feff222c853fb04d3c443
Thomas Steinacher <tom@eggdrop.ch>
2020-06-17 11:17:23 -0400

    Add Python 3.7

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=['tasktiger'],
     package_data={'tasktiger': ['lua/*.lua']},
