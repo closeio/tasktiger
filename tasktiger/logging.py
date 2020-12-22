6f29b6854c8bcfee0130eb7b3c8320a2334bafab
Joe Kemp <joe@close.io>
2020-12-22 16:00:14 -0500

    Fixup

--- tasktiger/logging.py
+++ tasktiger/logging.py
@@ -8,7 +8,7 @@ def tasktiger_processor(logger, method_name, event_dict):
     Inject the current task id for non-batch tasks.
     """
 
-    if not g['current_task_is_batch'] and g['current_tasks'] is not None and :
+    if not g['current_task_is_batch'] and g['current_tasks'] is not None:
         event_dict['task_id'] = g['current_tasks'][0].id
     elif g['current_task_is_batch'] and g['current_batch_task'] is not None:
         event_dict['task_id'] = g['current_batch_task'].id
