5b54a127c538b019af3690e4e7f6c0c8a9904fe1
Vojta Udrzal <vojta.udrzal@close.io>
2021-04-21 08:07:06 +0200

    v0.15 (#196)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = install_requires + ['freezefrog', 'pytest', 'psutil']
 
 setup(
     name='tasktiger',
-    version='0.14',
+    version='0.15',
     url='http://github.com/closeio/tasktiger',
     license='MIT',
     description='Python task queue',
